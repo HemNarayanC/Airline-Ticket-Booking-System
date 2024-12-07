5a87fc7ca219227e9c250d366537be2ad031d85e
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2024-12-08 04:51:15 +0545

    airline login authorization

--- auth/loginDB.php
+++ auth/loginDB.php
@@ -34,5 +34,37 @@
                 echo "No user found";
             }
         }
+
+        //Airline Login
+        $sqlAirlineCheck = "SELECT * FROM `pending_airline` WHERE `email` = '$userEmail'";
+        $resultAirline = mysqli_query($conn, $sqlAirlineCheck);
+
+        if ($resultAirline) {
+            # code...
+            $num = mysqli_num_rows($resultAirline);
+
+            if($num==1){
+                while($row=mysqli_fetch_assoc($resultAirline)){
+                    if(password_verify($password, $row['password'])){
+                        session_start();
+                        $_SESSION['loggedIn'] = true;
+                        $_SESSION['user-type'] = "airline";
+                        $_SESSION['email'] = $userEmail;
+                        echo "Successfully Logged In";
+                        header('Location: ../admin/adminDashboard.php');
+
+                        // exit();
+                    }
+
+                    else{
+                        echo "Password didn't match";
+                    }
+                }
+            }
+
+            else{
+                echo "No user found";
+            }
+        }
     }
 ?>
