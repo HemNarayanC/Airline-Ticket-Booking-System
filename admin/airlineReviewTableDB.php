e89afbda3ec48f34c04ab161bea4a4ec661e9346
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2024-12-08 05:28:03 +0545

    inserting data of pending airline to airline table after approval

--- admin/airlineReviewTableDB.php
+++ admin/airlineReviewTableDB.php
@@ -12,6 +12,8 @@
     }
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        //for approval
         if(isset($_POST['approve_id'])) {
             $approve_id = $_POST['approve_id'];
 
@@ -26,7 +28,45 @@
             else{
                 echo "Error updating status".mysqli_error();
             }
+
+
+            //fetching data and moving to airline table after appoval
+            $fetchApprovedQuery = "SELECT * FROM `pending_airline` WHERE  `pending_id` = '$approve_id'";
+            $fetchResult = mysqli_query($conn, $fetchApprovedQuery);
+            $numOfFetchedRows = mysqli_num_rows($fetchResult);
+
+            if($fetchResult && $numOfFetchedRows > 0) {
+                $row = mysqli_fetch_assoc($fetchResult);
+
+                //Inserting the approved data to airline table
+                $insertQuery = "INSERT INTO airline (`registration_no`,`c_name`,`email`,`phone`,`password`,`address`)
+                                VALUES('".$row['registration_no']."',
+                                        ".$row['c_name'].",
+                                        ".$row['email'].",
+                                        ".$row['phone'].",
+                                        ".$row['password'].",
+                                        ".$row['address'].")";
+
+                $resultInsertion = mysqli_query($conn, $resultInsertion);
+                if($resultInsertion){
+                    echo "Data inserted into airline table successfully";
+                }
+
+                else{
+                    echo "Error inserting into airline table".mysqli_error();
+                }
+            }
+
+            else{
+                echo "No data found for the pending ID: $approve_id";
+            }
         }
+
+        else{
+            echo "No record found for provided Id";
+        }
+
+        //for reject
     }
 
 
