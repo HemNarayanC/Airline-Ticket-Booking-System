c3d7b4b22ca8ac6d0977bca739f2077b35a9b4a9
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2024-12-09 10:41:37 +0545

    added table for displaying airport

---
+++ admin/manageAirport.php
@@ -0,0 +1,16 @@
+<div class="airport-display">
+    <h2>Airport List</h2>
+    <table class="airport-table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Airport Name</th>
+                <th>Location</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+</div>
