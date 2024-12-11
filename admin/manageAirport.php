c4900e3790865716ba3a5aa5099dc310ef50864f
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2024-12-11 06:52:59 +0545

    displaying data of airports in table

--- admin/manageAirport.php
+++ admin/manageAirport.php
@@ -1,4 +1,4 @@
-<div class="airport-display">
+<div class = "airport-display">
     <h2>Airport List</h2>
     <table class="airport-table">
         <thead>
@@ -10,7 +10,28 @@
             </tr>
         </thead>
         <tbody>
+            <?php
+                include('../partials/_db_connect.php');
+                $sqlAirportCheck = "SELECT * FROM `airport`";
+                $resultAirport = mysqli_query($conn, $sqlAirportCheck);
+                if($resultAirport && mysqli_num_rows($resultAirport)){
+                    while($row = mysqli_fetch_assoc($resultAirport)) {
+                        echo "<tr>";
+                        echo "<td>{$row['airport_id']}</td>";
+                        echo "<td>{$row['airport_name']}</td>";
+                        echo "<td>{$row['location']}</td>";
+                        echo "<td class = 'airport-action-btn'>";
+                            echo "<button class = 'airport-edit-btn'> Edit </button>";
+                            echo "<button class = 'airport-delete-btn'> Delete </button>";
+                        echo "</td>";
+                        echo "</tr>";
+                    }
+                }
 
+                else{
+                    echo "<tr><td colspan = '4'>No airports found</td></tr>";
+                }
+            ?>
         </tbody>
     </table>
 </div>
