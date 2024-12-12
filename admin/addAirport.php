532c733263136d1cd65c74a42f777e0c5fed524f
kalpana kabirath <kabirathkalpana10@gmail.com>
2024-12-12 06:47:10 +0545

    component for adding airport

---
+++ admin/addAirport.php
@@ -0,0 +1,11 @@
+
+    <div id="airportManagement" class="airport-management">
+        <h2>Airport Management</h2>
+        <form id="addAirportForm">
+            <input type="text" id="airportName" placeholder="Airport Name" required>
+            <input type="text" id="location" placeholder="Location" required>
+            <input type="text" id="areaCode" placeholder="Area Code" required>
+            <button type="submit">Add Airport</button>
+        </form>
+        <div id="message" class="message"></div>
+    </div>
