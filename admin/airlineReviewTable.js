842ae0e5312f76f5d8e1e159568c717b72c88fca
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2024-12-13 12:10:40 +0545

    toggle visibility of airport management and review table on selection/change

--- admin/airlineReviewTable.js
+++ admin/airlineReviewTable.js
@@ -1,8 +1,13 @@
 
+//displaying airline review table on changing option
 let tableContainer = document.querySelector('.airline-review-table');
 let manageAirlineSelect = document.getElementById('manageAirline');
 
 manageAirlineSelect.addEventListener('change', (event) => {
+
+    //Hide the airport component if its visible
+    let airportComponent = document.getElementsByClassName('airport-management')[0];
+    airportComponent.classList.remove('show-airport-component');
     if(event.target.value === 'review-registration'){
         tableContainer.classList.add('show-requests');
     }
@@ -11,3 +16,17 @@ manageAirlineSelect.addEventListener('change', (event) => {
         tableContainer.classList.remove('show-requests');
     }
 });
+
+
+//Displaying adding airport component when clicked
+let addAirportBtn = document.getElementById('add-airport-btn');
+let airportComponent = document.getElementsByClassName('airport-management')[0];
+addAirportBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    //hide the airline review table if it's visible
+    tableContainer.classList.remove('show-requests');
+    manageAirlineSelect.value = '';
+    airportComponent.classList.add('show-airport-component');
+});
+
