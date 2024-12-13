b63ce60e15e2808cd3e1a29733f789e76fa8c555
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2024-12-13 14:20:54 +0545

    sql query for adding airport to db

--- admin/addAirport.php
+++ admin/addAirport.php
@@ -1,10 +1,10 @@
 
     <div id="airportManagement" class="airport-management">
         <h2>Airport Management</h2>
-        <form id="addAirportForm">
-            <input type="text" id="airportName" placeholder="Airport Name" required>
-            <input type="text" id="location" placeholder="Location" required>
-            <input type="text" id="areaCode" placeholder="Area Code" required>
+        <form action = "addAirport.php" method = "POST" id="addAirportForm">
+            <input type="text" id="airportName" name="airportName" placeholder="Airport Name" required>
+            <input type="text" id="location" name="location" placeholder="Location" required>
+            <input type="text" id="areaCode" name="areaCode" placeholder="Area Code" required>
             <button type="submit">Add Airport</button>
         </form>
         <div class="airport-btn-container">
@@ -12,3 +12,26 @@
             <button id="hideAirportsBtn">Hide</button>
         </div>
     </div>
+
+    <?php
+        include('../partials/_db_connect.php');
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $airport_name = $_POST['airportName'];
+            $airport_location = $_POST['location'];
+            $area_code = $_POST['areaCode'];
+
+            $insertAirport = "INSERT INTO `airport`(`airport_name`, `location`, `area_code`)VALUES
+                                ('$airport_name', '$airport_location', '$area_code')";
+            $resultAddAirport = mysqli_query($conn, $insertAirport);
+
+            if($resultAddAirport){
+                echo "Airport Added Successfully";
+                header('Location: adminDashboard.php');
+            }
+
+            else{
+                echo "Unable to add Airport";
+            }
+        }
+
+    ?>
