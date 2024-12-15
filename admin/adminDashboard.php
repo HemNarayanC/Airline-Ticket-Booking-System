02a5377baf055933c5e8d0cd786772efbb890b7e
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2024-12-15 12:52:58 +0545

    Updated admin dashboard layout and simplified code by modifying links

--- admin/adminDashboard.php
+++ admin/adminDashboard.php
@@ -13,22 +13,7 @@
     $initialName = $_SESSION['nameInitial'];
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="adminDashboard.css">
-    <link rel="stylesheet" href="airlineReviewTable.css">
-    <link rel="stylesheet" href="manageAirport.css">
-    <link rel="stylesheet" href="addAirport.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-</head>
-<body>
-    
+ 
     <div class="dashboard">
         <!-- Left Sidebar -->
         <aside class="sidebar">
@@ -54,7 +39,7 @@
                     Flight Search
                 </a>
 
-                <a href="" class="nav-item" id="add-airport-btn">
+                <a href="addAirport.php" class="nav-item" id="add-airport-btn">
                     <i class="material-icons">
                     connecting_airports
                     </i>
@@ -76,13 +61,13 @@
                     </div>
                 </div>
 
-                <a href="" class="nav-item">
+                <a href="#" class="nav-item">
                     <i class="material-icons">
                     manage_accounts
                     </i>
                     Manage Users
                 </a>
-                <a href="" class="nav-item">
+                <a href="#" class="nav-item">
                     <i class="material-icons">
                     attach_money
                     </i>
@@ -96,111 +81,3 @@
                 </div>
             </div>
         </aside>
-
-
-        <!-- Main Content -->
-         <main class="main-content">
-            <!-- Header -->
-             <header class="header">
-                <div class="header-content">
-                    <div class="section-title">
-                        DASHBOARD
-                    </div>
-                    <div class="search-bar">
-                        <i class="material-icons">search</i>
-                        <input type="text" name="search" id="searchBar" placeholder="Search">
-                    </div>
-                </div>
-             </header>
-         
-
-            <div class="managing-container">
-                <div class="airline-review-table">
-                    <h1>Airline Registration Requests</h1>
-                    <table class="airline-table">
-                        <!-- table heading of airline table -->
-                        <thead>
-                            <tr>
-                                <th>Pending ID</th>
-                                <th>Registration No</th>
-                                <th>Company Name</th>
-                                <th>Email</th>
-                                <th>Phone</th>
-                                <th>Status</th>
-                                <th>Submitted At</th>
-                                <th>Address</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-
-                        <!-- body of airline review table -->
-                        <tbody>
-                            <?php
-                                $sqlAirlineCheck = "SELECT * FROM `pending_airline` WHERE `status` = 'pending'";
-                                $resultAirline = mysqli_query($conn, $sqlAirlineCheck);
-                                $numOfRows = mysqli_num_rows($resultAirline);
-
-                                if($numOfRows > 0) {
-                                    while($row = mysqli_fetch_assoc($resultAirline)){
-                                        echo "<tr>";
-                                            echo "<td>{$row['pending_id']}</td>";
-                                            echo "<td>{$row['registration_no']}</td>";
-                                            echo "<td>{$row['c_name']}</td>";
-                                            echo "<td>{$row['email']}</td>";
-                                            echo "<td>{$row['phone']}</td>";
-
-
-                                            $status = $row['status'];
-
-                                            if($status == 'pending'){
-                                                echo "<td><span class = 'status-pending'>Pending</span></td>";
-                                            }
-
-                                            elseif($status == 'approved'){
-                                                echo "<td><span class = 'status-approved'>approved</span></td>";
-                                            }
-
-                                            elseif($status == 'rejected'){
-                                                echo "<td><span class = 'status-rejected'>rejected</span></td>";
-                                            }
-
-                                            echo "<td>{$row['submitted_at']}</td>";
-                                            echo "<td>{$row['address']}</td>";
-                                            echo "<td>
-                                                    <form method = 'POST' action = 'airlineReviewTableDB.php' style = 'display: inline-block;'>
-                                                        <input type = 'hidden' name = 'approve_id' value = '{$row['pending_id']}'>
-                                                        <button type = 'submit' class = 'approve-btn'> Approve </button>
-                                                    </form>
-
-                                                    <form method = 'POST' action = 'airlineReviewTableDB.php' style = 'display: inline-block;'>
-                                                        <input type = 'hidden' name = 'reject_id' value = '{$row['pending_id']}'>
-                                                        <button type = 'submit' class = 'reject-btn'> Reject </button>
-                                                    </form>
-                                                </td>";
-
-                                        echo "</td>";
-                                    }
-                                }
-
-                                else{
-                                    echo "<tr><td colspan = '9'>No records found.</td></tr>";
-                                }
-                            ?>
-                        </tbody>
-                    </table>
-                </div>
-
-                <!-- table for managing airport -->
-                <?php
-                    include('addAirport.php');
-                    include('manageAirport.php');
-                ?>
-
-            </div>
-         </main>
-    </div>
-
-    <script src="airlineReviewTable.js"></script>
-    <script src="showAirports.js"></script>
-</body>
-</html>
