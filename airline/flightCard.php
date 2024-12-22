11fc2695f3f0874accfbeaccf32a6ef72a71bbe4
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2024-12-22 19:29:35 +0545

    added  return flight details in card

--- airline/flightCard.php
+++ airline/flightCard.php
@@ -58,7 +58,40 @@
                 </div>
             </div>
             <!-- Return Flight -->
-             
+            <div class="flight-route return flights">
+                <div class="route-info">
+                    <div class="departure">
+                        <div class="time">11:06 am</div>
+                        <div class="city">Pokhara, PKR</div>
+                        <div class="date">12 Oct, 2024, Mon</div>
+                    </div>
+                    
+                    <div class="flight-duration">
+                        <div class="duration-line">
+                            <span class="dot"></span>
+                            <span class="line"></span>
+                            <span class="dot"></span>
+                        </div>
+                    </div>
+                    
+                    <div class="arrival">
+                        <div class="time">9:45 am</div>
+                        <div class="city">Kathmandu, KTM</div>
+                        <div class="date">12 Oct 2024, Mon</div>
+                    </div>
+
+                    <div class="flight-details">
+                        <div class="detail">
+                            <span class="label">Seats Left:</span>
+                            <span class="value">10+</span>
+                        </div>
+                        <div class="detail">
+                            <span class="label">Flight:</span>
+                            <span class="value">U6-5084</span>
+                        </div>
+                    </div>
+                </div>
+            </div> 
 
          </div>
     </div>
