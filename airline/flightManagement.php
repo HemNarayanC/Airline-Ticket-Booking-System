b8426c59809af6951346f1f35113200623d58b5e
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2024-12-26 11:14:10 +0545

    added link for material icons + round

--- airline/flightManagement.php
+++ airline/flightManagement.php
@@ -6,6 +6,7 @@
     <title>Manage Flight</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <link rel="stylesheet" href="airlineDashboard.css">
     <link rel="stylesheet" href="flightManagement.css">
 </head>
