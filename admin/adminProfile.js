640271dd0e1cf0d22831939689c8a346afbb3429
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2024-12-27 13:05:27 +0545

    added eventlistener for submit btn

--- admin/adminProfile.js
+++ admin/adminProfile.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', (e) => {
     const editButton = document.querySelector('.edit-btn');
     const saveButton = document.querySelector('.save-btn');
+    const submitBtn = document.querySelector('.submit-btn');
 
     editButton.addEventListener('click', (e) => {
         let fields = editButton.closest('.section').querySelectorAll('input');
@@ -11,5 +12,13 @@ document.addEventListener('DOMContentLoaded', (e) => {
         });
         editButton.classList.add('hidden');
         saveButton.classList.remove('hidden');
+    });
+
+    saveButton.addEventListener('click', (e) => {
+        const fields = saveButton.closest('.section').querySelectorAll('input');
+        fields.forEach(field => field.setAttribute('readonly', true));
+        saveButton.classList.add('hidden');
+        editButton.classList.remove('hidden');
+        submitBtn.classList.remove('hidden');
     });
 });
