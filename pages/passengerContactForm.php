c15479691b442288ce9e1fc18dc73b2ad7a6fbe6
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2024-12-28 13:15:01 +0545

    added function to generate passenger section form

--- pages/passengerContactForm.php
+++ pages/passengerContactForm.php
@@ -10,6 +10,50 @@
         die("Invalid number of passengers. Please go back and try again");
     }
 
+    function generatePassengerSection($type, $number, $passenger_count){
+        $age_field = '';
+
+        if($type === 'CHILD'){
+            $age_field = '<div class="input-group">
+                            <input type = "number" id="age-'.$passenger_count.'" name="age-'.$passenger_count.' " min="2" max="11" placeholder = "Age (2-11 years)">
+                        </div>';
+        }
+
+        elseif ($type === 'INFANT') {
+        $age_field = '<div class="input-group">
+                        <input type="number" id="age-' . $passenger_count . '" name="age-' . $passenger_count . '" min="0" max="24" required placeholder="Age (in months)">
+                    </div>';
+        }
+
+        return '
+            <div class = "passenger-section">
+                <div class = "section-header"'. strtoupper($type).''.$number.'</div>
+                <div class = "input-row">
+                    <div class = "input-group">
+                        <input type = "text" id = "given-names-'.$passenger_count.'" name = "given-names-'.$passenger_count.'" placeholder = "First Name" required>
+                    </div>
+                    <div class = "input-group">
+                        <input type = "text" id = "nationality='.$passenger_count.'" name = "nationality-'.$passenger_count.'" placeholder = "Nationality" placeholder = "Nationality" required">
+                    </div>
+
+                    <div class = "gender-group">
+                        <label class = "gender-label">Gender</label>
+                        <div class = "radio-options">
+                            <label class="radio-label">
+                                <input type = "radio" name = "gender-'.$passenger_count.'" value = "male" required>
+                                Male
+                            </label>
+                            <label class="radio-label">
+                                <input type = "radio" name = "gender-' . $passenger_count . '" value= "female">
+                                Female
+                            </label>
+                        </div>
+                    </div>
+                    '.$age_field.'
+                </div>
+                ';
+}
+
 ?>
 
 <!DOCTYPE html>
